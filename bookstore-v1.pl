cba7a7379fb1df2c183d381cb26a7e6ec9615330
May Young <youngmaia5@gmail.com>
2016-10-16 17:32:34 -0700

    Fixed is queries

--- bookstore-v1.pl
+++ bookstore-v1.pl
@@ -18,13 +18,6 @@ noun_phrase(T0,T4,Ind,C0,C4) :-
     noun(T2,T3,Ind,C2,C3),
     mp(T3,T4,Ind,C3,C4).
 
-opt_noun_phrase(T0,T4,Ind,C0,C4) :-
-    det(T0,T1,Ind,C0,C1),
-    adjectives(T1,T2,Ind,C1,C2),
-    noun(T2,T3,Ind,C2,C3),
-    mp(T3,T4,Ind,C3,C4).
-opt_noun_phrase(T,T,_,C,C).
-
 % Determiners (articles) are ignored in this oversimplified example.
 % They do not provide any extra constaints.
 det([the | T],T,_,C,C).
@@ -43,17 +36,12 @@ adjectives(T,T,_,C,C).
 % a relation (verb or preposition) followed by a noun_phrase or
 % 'that' followed by a relation then a noun_phrase or
 % nothing 
-mp(T0,T1,I1,C0,C1) :-
-    reln(T0,T1,I1,_,C0,C1).
-mp(T0,T2,I1,C0,C2) :-
-    reln(T0,T1,I1,_,C0,C1),
-    noun(T1,T2,I1,C1,C2).
 mp(T0,T2,I1,C0,C2) :-
     reln(T0,T1,I1,I2,C0,C1),
     noun_phrase(T1,T2,I2,C1,C2).
 mp([that|T0],T2,I1,C0,C2) :-
     reln(T0,T1,I1,I2,C0,C1),
-    opt_noun_phrase(T1,T2,I2,C1,C2).
+    noun_phrase(T1,T2,I2,C1,C2).
 mp(T,T,_,C,C).
 
 
@@ -66,86 +54,47 @@ noun([author | T],T,Ind,C,[author(Ind)|C]).
 noun([book | T],T,Ind,C,[book(Ind)|C]).
 noun([publisher | T],T,Ind,C,[publisher(Ind)|C]).
 % The following are for proper nouns:
-noun([Ind | T],T,[Ind],C,C) :- book([Ind]).
-noun([Ind1, Ind2 | T],T,[Ind1, Ind2],C,C) :- book([Ind1, Ind2]).
-noun([IndF, IndL | T],T,[IndF, IndL],C,C) :- author([IndF, IndL]).
-noun([Ind | T],T,[Ind],C,C) :- publisher([Ind]).
-noun([Ind1, Ind2 | T],T,[Ind1, Ind2],C,C) :- publisher([Ind1, Ind2]).
+noun([Ind | T],T,Ind,C,C) :- author(Ind).
+noun([Ind | T],T,Ind,C,C) :- book(Ind).
+noun([Ind | T],T,Ind,C,C) :- publisher(Ind).
 
 % adj(T0,T1,Ind,C0,C1) is true if T0-T1 is an adjective that provides properties C1-C0 to Ind
 adj([fiction | T],T,Ind,C,[fiction(Ind)|C]).
 adj([historical | T],T,Ind,C,[historical(Ind)|C]).
 adj([cost | T],T,Ind,C,[price(Ind,_)|C]).
 adj([long | T],T,Ind,C,[num_pages(Ind,_)|C]).
+adj([more,than | T],[_,pages | T],Ind,C,[num_pages(Ind,_)|C]).
+adj([less,than | T],[_,pages | T],Ind,C,[num_pages(Ind,_)|C]).
+adj([more,than | T],[_,dollars | T],Ind,C,[price(Ind,_)|C]).
+adj([less,than | T],[_,dollars | T],Ind,C,[price(Ind,_)|C]).
 
 % reln(T0,T1,I1,I2,R0,R1) is true if T0-T1 is a relation
 %   that provides relations R1-R0 on individuals I1 and I2
 reln([wrote | T],T,I1,I2,C,[wrote(I1,I2)|C]).
-reln([published, by | T],T,I1,I2,C,[published(I2,I1)|C]).
-reln([by | T],T,I1,I2,C,[wrote(I2,I1)|C]).
+reln([by | T],T,I1,I2,C,[wrote(I1,I2)|C]).
 reln([published | T],T,I1,I2,C,[published(I1,I2)|C]).
 reln([costs | T],T,I1,I2,C,[price(I1,I2)|C]).
-reln([the, author, of | T],T,I1,I2,C,[wrote(I1,I2)|C]).
-reln([the, publisher, of | T],T,I1,I2,C,[published(I1,I2)|C]).
-reln([more,than,X,pages | T],T,I1,I2,C,[num_pages(I1,I2), number(X), more_than_pages(I1,X)|C]).
-reln([less,than,X,pages | T],T,I1,I2,C,[num_pages(I1,I2), number(X), less_than_pages(I1,X)|C]).
-reln([more,than,X,dollars | T],T,I1,I2,C,[price(I1,I2), number(X), more_than_dollars(I1,X)|C]).
-reln([less,than,X,dollars | T],T,I1,I2,C,[price(I1,I2), number(X), less_than_dollars(I1,X)|C]).
-
-reln([has,more,than,X,pages | T],T,I1,I2,C,[num_pages(I1,I2), number(X), more_than_pages(I1,X)|C]).
-reln([has,less,than,X,pages | T],T,I1,I2,C,[num_pages(I1,I2), number(X), less_than_pages(I1,X)|C]).
-reln([has,more,than,X,dollars | T],T,I1,I2,C,[price(I1,I2), number(X), more_than_dollars(I1,X)|C]).
-reln([has,less,than,X,dollars | T],T,I1,I2,C,[price(I1,I2), number(X), less_than_dollars(I1,X)|C]).
-
+reln([author,of | T],T,I1,I2,C,[wrote(I1,I2)|C]).
+reln([publisher,of | T],T,I1,I2,C,[published(I1,I2)|C]).
+reln([published, by | T],T,I1,I2,C,[published(I1,I2)|C]).
 
 % question(Question,QR,Indect,Q0,Query) is true if Query-Q0 provides an answer about Indect to Question-QR
 question([what,is | T0],T1,Ind,C0,C1) :-
     mp(T0,T1,Ind,C0,C1).
 question([what,is | T0],T1,Ind,C0,C1) :-
     noun_phrase(T0,T1,Ind,C0,C1).
-question([what,is| T0],T2,Obj,C0,C2) :-
-    noun_phrase(T0,T1,Obj,C0,C1),
-    mp(T1,T2,Obj,C1,C2).
-question([what,is| T0],T2,Obj,C0,C2) :-
-    mp(T0,T1,Obj,C0,C1),
-    noun_phrase(T1,T2,Obj,C1,C2).
-
 question([is | T0],T2,Ind,C0,C2) :-
     noun_phrase(T0,T1,Ind,C0,C1),
     mp(T1,T2,Ind,C1,C2).
-question([is | T0],T3,Ind,C0,C3) :-
-    noun_phrase(T0,T1,Ind,C0,C1),
-    noun_phrase(T1,T2,Ind,C1,C2),
-    mp(T2,T3,Ind,C2,C3).
-
 question([who,is | T0],T1,Ind,C0,C1) :-
     mp(T0,T1,Ind,C0,C1).
 question([who,is | T0],T1,Ind,C0,C1) :-
     noun_phrase(T0,T1,Ind,C0,C1).
 question([who,is | T0],T1,Ind,C0,C1) :-
     adjectives(T0,T1,Ind,C0,C1).
-question([who,is| T0],T2,Obj,C0,C2) :-
-    noun_phrase(T0,T1,Obj,C0,C1),
-    mp(T1,T2,Obj,C1,C2).
-question([who,is| T0],T2,Obj,C0,C2) :-
-    mp(T0,T1,Obj,C0,C1),
-    noun_phrase(T1,T2,Obj,C1,C2).
-
-question([who | T0],T1,Ind,C0,C1) :-
-    mp(T0,T1,Ind,C0,C1).
-question([who | T0],T1,Ind,C0,C1) :-
-    noun_phrase(T0,T1,Ind,C0,C1).
-question([who | T0],T2,Obj,C0,C2) :-
-    noun_phrase(T0,T1,Obj,C0,C1),
-    mp(T1,T2,Obj,C1,C2).
-question([who | T0],T2,Obj,C0,C2) :-
-    mp(T0,T1,Obj,C0,C1),
-    noun_phrase(T1,T2,Obj,C1,C2).
-
 question([what | T0],T2,Ind,C0,C2) :-      % allows for a "what ... is ..."
     noun_phrase(T0,[is|T1],Ind,C0,C1),
     mp(T1,T2,Ind,C1,C2).
-
 question([what | T0],T2,Ind,C0,C2) :-
     noun_phrase(T0,T1,Ind,C0,C1),
     mp(T1,T2,Ind,C1,C2).
@@ -171,10 +120,6 @@ prove_all([H|T]) :-
 %
 
 % author([F,L]) is true if F is a first name and L is a last name of an author
-%% author(F) :- author_full_name(F,_).
-%% author(L) :- author_full_name(_,L).
-%% author_full_name(emma,donoghue).
-%% author([_,_]).
 author([emma,donoghue]).
 
 % book(T) is true if T is a list representing the title of a book
@@ -189,22 +134,21 @@ publisher([harperCollins]).
 %
 
 % wrote(A,B) is true if author A wrote book B
-wrote([emma,donoghue], [the,wonder]).
-
+wrote(author([emma,donoghue]), book([the,wonder])).
 
 % published(P,B) is true if publisher P published book B
-published([harperCollins], [the,wonder]).
+published(publisher([harperCollins]), book([the,wonder])).
 
 
 %
-% Adjectives (adjective)`
+% Adjectives (adjective)
 %
 
 % num_pages(B,N) is true if book B has N number of pages
-num_pages([the,wonder],304).
+num_pages(book([the,wonder]),304).
 
 % price(B,N) is true if book B costs N dollars
-price([the,wonder],33).
+price(book([the,wonder]),33).
 
 % more_than_dollars(B,N) is true if book B costs more than N dollars
 more_than_dollars(B,N) :- price(B,C), C > N.
@@ -224,11 +168,11 @@ less_than_pages(B,N) :- num_pages(B,L), L < N.
 %
 
 % fiction(B) is true if book B is a fiction book
-fiction([the,wonder]).
+fiction(book([the,wonder])).
 
 % non_fiction(B) is true if book B is a non-fiction book
 % TODO
 
 % historical(B) is true if book B is a historical book
-historical([the,wonder]).
+historical(book([the,wonder])).
 
